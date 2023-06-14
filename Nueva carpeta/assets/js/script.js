8f3649380cc405cb21e92409ed72f92d391de7d5
Takerin <96486976+Taker-Miller@users.noreply.github.com>
2023-06-14 18:19:21 -0400

    Update script.js

--- Nueva carpeta/assets/js/script.js
+++ Nueva carpeta/assets/js/script.js
@@ -1,45 +1,32 @@
-var changeContraste = ()=> {
+// script.js
+var changeContraste = () => {
     let btn = document.getElementById("btnContraste");
     let estado = btn.value;
-    if (estado =="o"){
-        btn.value ="c";
+    if (estado == "o") {
+        btn.value = "c";
         let elements = document.getElementsByClassName('negro');
-        console.log(elements)
-        console.log(elements[0])
-        console.log(elements [0].classList);
         elements[0].classList.add('blanco');
-        console.log(elements [0].classList);
         elements[0].classList.remove('negro');
-        console.log(elements [0].classList);
-      
-    }
-    else if 
-        (estado =="c"){
-            btn.value ="o";
-            let elements = document.getElementsByClassName('blanco');
-        console.log(elements)
-        console.log(elements[0])
+    } else if (estado == "c") {
+        btn.value = "o";
+        let elements = document.getElementsByClassName('blanco');
         elements[0].classList.add('negro');
         elements[0].classList.remove('blanco');
-        }
-    
-        
     }
+}
 
-var changeFuente = ()=> {
+var changeFuente = () => {
     let btn = document.getElementById("btnFuente");
     let estado = btn.value;
-    if (estado=='0'){
-        btn.value ='1';
+    if (estado == '0') {
+        btn.value = '1';
         let elements = document.getElementsByClassName("small");
         for (let index = 0; index < elements.length; index++) {
             const element = elements[index];
-            element.classList.add("medum");
+            element.classList.add("medium");
             element.classList.remove("small");
         }
-}
-
-    else if (estado =='1'){
+    } else if (estado == '1') {
         btn.value = '2';
         let elements = document.getElementsByClassName("medium");
         for (let index = 0; index < elements.length; index++) {
@@ -47,21 +34,16 @@ var changeFuente = ()=> {
             element.classList.add("large");
             element.classList.remove("medium");
         }
-      
-    }
-    
-    else if (estado =='2'){
-        btn.value = '0'
+    } else if (estado == '2') {
+        btn.value = '0';
         let elements = document.getElementsByClassName("large");
-        console.log(elements)
         for (let index = 0; index < elements.length; index++) {
             const element = elements[index];
             element.classList.add("small");
             element.classList.remove("large");
-    }
-    alert("hola")
-
+        }
     }
 }
+
 document.getElementById("btnContraste").addEventListener("click", changeContraste);
 document.getElementById("btnFuente").addEventListener("click", changeFuente);
